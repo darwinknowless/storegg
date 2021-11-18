4d6ea9e6bf41f49a9f34f860bbfe92b606a5a2e9
darwinknowless <id.darwins@gmail.com>
2021-11-18 15:44:25 +0700

    Convert Complete-Checkout Page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/utilities.css'; // Import style here
 import '../styles/homepage.css'; // Import style here
 import '../styles/detail.css'; // Import style here
 import '../styles/checkout.css'; // Import style here
+import '../styles/complete-checkout.css'; // Import style here
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
