71267313bb9e9a39f9db1d7f83c2b56513deb4f6
darwinknowless <id.darwins@gmail.com>
2021-11-22 20:21:41 +0700

    create storybook components

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,32 @@
+import '../styles/utilities.css'; // Import style here
+import '../styles/homepage.css'; // Import style here
+import '../styles/detail.css'; // Import style here
+import '../styles/checkout.css'; // Import style here
+import '../styles/complete-checkout.css'; // Import style here
+import '../styles/sign-in.css'; // Import style here
+import '../styles/sign-up.css'; // Import style here
+import '../styles/sign-up-photo.css'; // Import style here
+import '../styles/sign-up-success.css'; // Import style here
+import '../styles/404-not-found.css'; // Import style here
+import '../styles/sidebar.css'; // Import style here
+import '../styles/overview.css'; // Import style here
+import '../styles/transactions.css'; // Import style here
+import '../styles/transactions-detail.css'; // Import style here
+import '../styles/edit-profile.css'; // Import style here
+import '../styles/navbar-log-in.css'; // Import style here
+import * as nextImage from 'next/image';
+
+Object.defineProperty(nextImage, 'default', {
+	configurable: true,
+	value: (props) => <img {...props} />,
+});
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
