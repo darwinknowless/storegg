bfe4f6943603e967c711a76ba398b7aa3ba6cdab
darwinknowless <id.darwins@gmail.com>
2021-12-22 03:54:48 +0700

    API Integration BE to FE. Install Axios. Simple Integration Next.Js

--- components/organisms/FeaturedGame/index.tsx
+++ components/organisms/FeaturedGame/index.tsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import GameItem from '../../molecules/GameItem';
 
 export default function FeaturedGame() {
+	// State
+	const [gameList, setGameList] = useState([]);
+	// Call API from BE
+	useEffect(async () => {
+		const response = await axios.get(
+			'https://bwa-storegg-backend.herokuapp.com/api/v1/players/landingpage'
+		);
+		console.log('data: ', response.data);
+		setGameList(response.data.data);
+	}, []);
 	return (
 		<section className='featured-game pt-50 pb-50'>
 			<div className='container-fluid'>
@@ -12,31 +24,16 @@ export default function FeaturedGame() {
 					className='d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4'
 					data-aos='fade-up'
 				>
-					<GameItem
-						title='Super Mechs'
-						category='Mobile'
-						thumbnail='/img/Thumbnail-1.png'
-					/>
-					<GameItem
-						title='Call of Duty: Modern'
-						category='Mobile'
-						thumbnail='/img/Thumbnail-2.png'
-					/>
-					<GameItem
-						title='Mobile Legends'
-						category='Mobile'
-						thumbnail='/img/Thumbnail-3.png'
-					/>
-					<GameItem
-						title='Clash of Clans'
-						category='Mobile'
-						thumbnail='/img/Thumbnail-4.png'
-					/>
-					<GameItem
-						title='Valorant'
-						category='Mobile'
-						thumbnail='/img/Thumbnail-5.png'
-					/>
+					{gameList.map((item) => {
+						return (
+							<GameItem
+								key={item._id}
+								title={item.name}
+								category={item.category.name}
+								thumbnail={`https://bwa-storegg-backend.herokuapp.com/uploads/${item.thumbnail}`}
+							/>
+						);
+					})}
 				</div>
 			</div>
 		</section>
