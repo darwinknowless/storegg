d04c5b6cbd226580959c9e92edef1618842c9f08
darwinknowless <id.darwins@gmail.com>
2022-01-07 04:11:14 +0700

    Handle Private Route in NextJS

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,10 +1,17 @@
 import Image from 'next/image';
+import jwtDecode from 'jwt-decode';
 import CheckoutItem from '../components/organisms/CheckoutItem';
 import CheckoutDetail from '../components/organisms/CheckoutDetail';
 import CheckoutConfirmation from '../components/organisms/CheckoutConfirmation';
+import { JWTPayloadTypes, UserTypes } from '../services/data-types';
 
-// Checkout Page
-export default function Checkout() {
+interface CheckoutProps {
+	user: UserTypes;
+}
+
+export default function Checkout(props: CheckoutProps) {
+	const { user } = props;
+	console.log('user: ', user);
 	return (
 		<section className='checkout mx-auto pt-md-100 pb-md-145 pt-30 pb-30'>
 			<div className='container-fluid'>
@@ -26,4 +33,29 @@ export default function Checkout() {
 			</div>
 		</section>
 	);
+}
+
+export async function getServerSideProps({ req }) {
+	const { token } = req.cookies;
+	// If no token, redirect to login
+	if (!token) {
+		return {
+			redirect: {
+				destination: '/sign-in',
+				permanent: false,
+			},
+		};
+	}
+	// Decode token
+	const jwtToken = Buffer.from(token, 'base64').toString('ascii'); // server side
+	const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+	const userFromPayload: UserTypes = payload.player;
+	const IMG = process.env.NEXT_PUBLIC_IMG;
+	userFromPayload.avatar = `${IMG}/${userFromPayload.avatar}`;
+	return {
+		props: {
+			// props for page
+			user: userFromPayload,
+		},
+	};
 }
